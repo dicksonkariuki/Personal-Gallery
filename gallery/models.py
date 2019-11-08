46731ecfe539e9074a172c41258881b8bac956fe
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-08 20:50:28 +0300

    Add location model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=20)
+
+# class Image(models.Model):
+#     name = models.CharField(max_length = 30)
+#     Description = models.TextField()
+#     location = models.ForeignKey(Location)
+#     category = models.ManyToManyField(Categorys, default = True)
+#     pub_date = models.DateTimeField(auto_now_add=True, null=True)
+#     Image_image = models.ImageField(upload_to = 'images/')
