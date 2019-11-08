1e26cc3838e989183237775094713bad5be12e23
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-08 21:24:23 +0300

    Add save and delete_location settings

--- gallery/models.py
+++ gallery/models.py
@@ -3,6 +3,16 @@ import datetime as dt
 
 class Location(models.Model):
     name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering=['name']
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        sel.delete()
 
 # class Image(models.Model):
 #     name = models.CharField(max_length = 30)
