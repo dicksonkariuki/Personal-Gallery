15df74d437e0446e9e5e9b6639a8b5f860772171
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-10 15:31:45 +0300

    Add image url

--- gallery/models.py
+++ gallery/models.py
@@ -37,7 +37,7 @@ class Location(models.Model):
 class Image(models.Model):
     name = models.CharField(max_length=20)
     description = models.TextField()
-    location = models.ForeignKey('Location',on_delete=models.PROTECT )
+    location = models.ForeignKey('Location',on_delete=models.PROTECT,default= True )
     category = models.ManyToManyField(Categorys, default = True)
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
     Image_image = models.ImageField(upload_to = 'images/')
