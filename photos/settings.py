b69f61a5535c7c946585a880aad56007bfcc0055
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-10 21:14:01 +0300

    Add a requirements file

--- photos/settings.py
+++ photos/settings.py
@@ -125,11 +125,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# static files settings
 STATIC_URL = '/static/'
+#Location to store static files and bootstrap
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# Location where django collect all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # configuring the location for media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
