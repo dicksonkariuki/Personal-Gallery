f1c8f22d984a0a70bd2d47dd15b7f7f585f19b17
Dickson Karis <dicksonkariuki4@gmail.com>
2019-11-10 15:41:40 +0300

    Set up an instance of image

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+import datetime as dt
+from .models import Image,Category,Location
+
+class ImageTestClass(TestCase):
+    """
+    Creating an instance of Image
+    """
+    def setUp(self):
+        self.pic = Image(name ='Movie',description='Watching movies is the best way to spend free time')
 
 # Create your tests here.
